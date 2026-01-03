08e6a663b0b6cd85574c55078adf7e15def90006
tuantq29 <tuantq29@fpt.com>
2026-01-03 22:14:58 +0700

    Add ingress & health module

--- apps/auth/src/auth.service.ts
+++ apps/auth/src/auth.service.ts
@@ -10,7 +10,10 @@ export class AuthService {
   constructor(
     private readonly configService: ConfigService,
     private readonly jwtService: JwtService,
-  ) {}
+  ) {
+    console.log(this.configService);
+  }
+
 
   async login(user: UserDocument, response: Response) {
     const tokenPayload: TokenPayload = {
